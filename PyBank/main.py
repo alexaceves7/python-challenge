bc017b6698386f9a0ef219eae399ce85f3b3ce52
alexaceves7 <alexaceves7@gmail.com>
2018-09-04 18:07:03 -0700

    Adding HW files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,59 @@
 #main.py for PyBank
+
+#add imports
+import os
+import csv
+months = 0
+total_profit = 0
+month_change = 0
+previous_month = 0
+change = []
+d = {}
+
+#read csv file
+csvpath = os.path.join('.','Resources','budget_data.csv')
+
+#split up rows and add delimiter
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    next (csvreader) #moves to next row to avoide header
+    count = 0
+    for row in csvreader: #going to go through each row
+        months = months + 1 #keeps count of total months
+        total_profit = total_profit + int(row[1]) #adds up total
+        month_change = float(row[1]) - previous_month #finds the monthly change
+        if previous_month != 0: #to avoid including the first row of data since no previous month data
+            change.append(month_change) #appends to change 
+            d[str(row[0])] = round(month_change, 1)#adds change and the month to a dictionary
+        previous_month = float(row[1]) #makes the current month the previous month before going to next row
+        
+    
+    
+    
+    #calculates average change and saves it to varisble
+    average_change = round((sum(change)/len(change)), 2)
+   
+    
+    #gets the max and min from the d dictionary
+    maximum = max(d, key=d.get) 
+          
+    minimum = min(d, key=d.get)
+
+# prints everything to pybank txt file
+with open("pybank.txt", "w") as text_file:
+    print ('Financial Analysis', file=text_file)
+    print ('----------------------------', file=text_file)
+    print (f'Total Months: {months}',file=text_file)
+    print (f'Total : ${total_profit}',file=text_file)
+    print (f'Average Change: ${average_change}',file=text_file)
+    print (f'Greatest Increase in Profits: {maximum} (${int(d[maximum])})',file=text_file)
+    print (f'Greatest Decrease in Profits: {minimum} (${int(d[minimum])})',file=text_file)
+
+#prints evrything to terminal
+print ('Financial Analysis')
+print ('----------------------------')
+print (f'Total Months: {months}')
+print (f'Total : ${total_profit}')
+print (f'Average Change: ${average_change}')
+print (f'Greatest Increase in Profits: {maximum} (${int(d[maximum])})')
+print (f'Greatest Decrease in Profits: {minimum} (${int(d[minimum])})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,78 @@
 #main.py for PyPoll
+
+#add imports
+import os
+import csv
+#set up all variables I will need
+total_votes = 0 #keep count of votes
+khan = 0 #keep count of khan votes
+correy = 0 #keep count of correy votes
+li = 0 #keep count of li votes
+otooly = 0 #keep count pf otooly votes
+candidates = [] #list for candidates
+d = {} #dictionary for results to gert max
+
+#read csv file
+csvpath = os.path.join('.','Resources','election_data.csv')
+
+with open(csvpath, newline='') as csvfile: #will go through each row
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csvreader)
+    for row in csvreader:
+        total_votes = total_votes + 1 #adds to vote total
+        if row[2] not in candidates: #adds to candidate list
+            candidates.append(row[2])
+            
+        
+        if row[2] == "Khan": #counts only khan votes
+            khan = khan + 1
+        elif row[2] == "Correy": #counts only correy votes
+            correy = correy + 1
+        elif row[2] == "Li": #counts only li votes
+            li = li + 1
+        else:
+            otooly = otooly + 1 #counts only ottoley votes
+        
+        
+        #finds percent of total for each candidate and stores it
+        percent_khan = '{percent:.2%}'.format(percent=khan/total_votes)
+        percent_correy = '{percent:.2%}'.format(percent=correy/total_votes)    
+        percent_li = '{percent:.2%}'.format(percent=li/total_votes)
+        percent_otooly = '{percent:.2%}'.format(percent=otooly/total_votes)
+        
+        
+        #adds to dictionary the candidate, percent and total votes
+        d["Khan"] = [percent_khan, khan]
+        d["Correy"] = [percent_correy, correy]
+        d["Li"] = [percent_li, li]
+        d["O'Tooley"] = [percent_otooly, otooly]
+    
+#finds max from the dictionary
+maximum = max(d, key=d.get)
+
+#prints to pypoll txt file
+with open("pypoll.txt", "w") as text_file:
+    print ('Election Results', file=text_file)
+    print ('---------------------------', file=text_file)
+    print (f'Total Votes: {total_votes}', file=text_file)
+    print ('---------------------------', file=text_file)
+    print (f'Khan: {percent_khan} ({khan})', file=text_file)
+    print (f'Correy: {percent_correy} ({correy})', file=text_file)
+    print (f'Li: {percent_li} ({li})', file=text_file)
+    print (f"O'Tooley: {percent_otooly} ({otooly})", file=text_file)
+    print ('---------------------------', file=text_file)
+    print (f'Winner: {maximum}', file=text_file)
+    print ('---------------------------', file=text_file)
+
+#prints to terminal
+print ('Election Results')
+print ('---------------------------')
+print (f'Total Votes: {total_votes}')
+print ('---------------------------')
+print (f'Khan: {percent_khan} ({khan})')
+print (f'Correy: {percent_correy} ({correy})')
+print (f'Li: {percent_li} ({li})')
+print (f"O'Tooley: {percent_otooly} ({otooly})")
+print ('---------------------------')
+print (f'Winner: {maximum}')
+print ('---------------------------')
